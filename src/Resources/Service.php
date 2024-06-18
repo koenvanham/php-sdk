ba9c016927aecaac1b273f503b91bf000748f439
koenvanham <koenvanham@gmail.com>
2024-06-18 12:22:23 +0200

    Service added function listByParams

--- src/Resources/Service.php
+++ src/Resources/Service.php
@@ -22,4 +22,33 @@ class Service extends Resource
     {
         return $this->connection->get("/carriers/{$carrierId}/services");
     }
+
+    /**
+     * List services associated with a carrier based on $country, $product_type, and $delivery_type
+     *
+     * Display all services associated with a carrier in a list.
+     *
+     * @param int $carrierId The id of the carrier
+     * @param string $country Country code
+     * @param int $product_type 1 for mailbox package, 2 for package, 3 for pallet
+     * @param int $delivery_type 1 for address, 2 for parcel shop
+     * @return array<string, mixed|array<string|mixed>>
+     * @throws GuzzleException
+     * @throws ApiException
+     * @see https://app.sendy.nl/api/docs#tag/Services/operation/getCarrierServices
+     */
+    public function listByParams(int $carrierId, string $country = null, int $product_type = null, int $delivery_type = null): array
+    {
+        $params = [];
+        if($country !== null){
+            $params['country'] = $country;
+        }
+        if($product_type !== null){
+            $params['product_type'] = $product_type;
+        }
+        if($delivery_type !== null){
+            $params['delivery_type'] = $delivery_type;
+        }
+        return $this->connection->get("/carriers/{$carrierId}/services", $params);
+    }
 }
